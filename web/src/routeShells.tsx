b1ee6cfb5e99f18a0e493df8b482312cfb3ad786
gnana997 <gnana097@gmail.com>
2026-06-08 03:40:45 +0530

    feat(page-header): integrate OpenClusterShellButton and remove ClusterHeader component

--- web/src/routeShells.tsx
+++ web/src/routeShells.tsx
@@ -14,7 +14,6 @@ import { Suspense, useEffect, useRef } from "react";
 import { Outlet, useParams } from "react-router-dom";
 import { useQueryClient } from "@tanstack/react-query";
 import { Brand } from "./components/shell/Brand";
-import { ClusterHeader } from "./components/shell/ClusterHeader";
 import { ClusterRail } from "./components/shell/ClusterRail";
 import { Sidebar } from "./components/shell/Sidebar";
 import { queryKeys } from "./lib/queryKeys";
@@ -54,10 +53,7 @@ export function AppShell() {
         </div>
       </div>
       <main className="flex min-w-0 flex-1 flex-col bg-bg">
-        {cluster && <ClusterHeader cluster={cluster} />}
-        <div className="min-h-0 flex-1">
-          <Suspense fallback={<LoadingState resource="page" />}><Outlet /></Suspense>
-        </div>
+        <Suspense fallback={<LoadingState resource="page" />}><Outlet /></Suspense>
       </main>
     </div>
   );
